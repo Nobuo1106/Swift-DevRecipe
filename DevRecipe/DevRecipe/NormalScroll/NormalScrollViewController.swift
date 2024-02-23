9a4a08062fb44081c66aaae1daadb0eb085d503b
五十嵐伸雄 <gonnbe1106@gmail.com>
2024-02-23 13:48:02 +0900

    Normal ScrollViewAdded

---
+++ DevRecipe/DevRecipe/NormalScroll/NormalScrollViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NormalScrollViewController.swift
+//  DevRecipe
+//
+//  Created by 五十嵐伸雄 on 2024/02/23.
+//
+
+import UIKit
+
+class NormalScrollViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
