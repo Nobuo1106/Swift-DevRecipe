a27335dacc023c082e0f363600ed556aa7fe504d
五十嵐伸雄 <gonnbe1106@gmail.com>
2024-02-29 19:26:20 +0900

    Added Normal ActivityViewController

---
+++ DevRecipe/DevRecipe/NormalActivity/NormalActivityViewController.swift
@@ -0,0 +1,43 @@
+//
+//  NormalActivityViewController.swift
+//  DevRecipe
+//
+//  Created by 五十嵐伸雄 on 2024/02/29.
+//
+
+import UIKit
+
+class NormalActivityViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func share() {
+        let shareImage = UIImage(named: "sample")
+        guard let image = shareImage,
+            let imageData = image.jpegData(compressionQuality: image.scale) else {
+            return
+        }
+        
+        let activity = UIActivityViewController(
+            activityItems: [imageData],
+            applicationActivities: nil
+        )
+        activity.excludedActivityTypes = [
+            UIActivity.ActivityType.assignToContact,
+            UIActivity.ActivityType.print,
+            UIActivity.ActivityType(rawValue: "com.apple.NanoTimeKitCompanion.CreateWatchFace"),
+        ]
+        activity.popoverPresentationController?.sourceView = button
+        activity.popoverPresentationController?.sourceRect = button.bounds
+
+        // 3
+        present(activity, animated: true, completion: nil)
+    }
+    @IBAction func buttonDidTap(_ sender: Any) {
+        share()
+    }
+}
